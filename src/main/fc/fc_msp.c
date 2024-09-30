7ff816cd6eddd2cc6d55edada921adeb6d3b09d5
Jonas Vögel <jonasvogel@MacBookAir.net>
2024-09-30 22:52:00 +0200

    VTOL setup, before it was multirotor. changes in fc_msp.c

--- src/main/fc/fc_msp.c
+++ src/main/fc/fc_msp.c
@@ -912,8 +912,8 @@ static bool mspFcProcessOutCommand(uint16_t cmdMSP, sbuf_t *dst, mspPostProcessF
         sbufWriteU32(dst, gpsSol.llh.lon);
         sbufWriteU16(dst, gpsSol.llh.alt/100); // meters
         sbufWriteU16(dst, gpsSol.groundSpeed);
-        // sbufWriteU16(dst, gpsSol.groundCourse);
-        sbufWriteU16(dst, posControl.flags.isGCSAssistedNavigationEnabled ? -3210 : gpsSol.groundCourse); // -3210 for redock
+        sbufWriteU16(dst, gpsSol.groundCourse);
+        // sbufWriteU16(dst, posControl.flags.isGCSAssistedNavigationEnabled ? -3210 : gpsSol.groundCourse); // -3210 for redock
         sbufWriteU16(dst, gpsSol.hdop);
         break;
 
@@ -2748,27 +2748,27 @@ static mspResult_e mspFcProcessInCommand(uint16_t cmdMSP, sbuf_t *src)
             radar_pois[msp_radar_no].heading = sbufReadU16(src);                   // °
             radar_pois[msp_radar_no].speed = sbufReadU16(src);                     // cm/s
             radar_pois[msp_radar_no].lq = sbufReadU8(src);                         // Link quality, from 0 to 4
-            // set nearest radar_poi as desired position if in poshold for multirotor:
-            if (
-                posControl.navState == NAV_STATE_POSHOLD_3D_IN_PROGRESS &&
-                ARMING_FLAG(ARMED) &&
-                (posControl.flags.estPosStatus == EST_TRUSTED) &&
-                posControl.gpsOrigin.valid
-            ){
-                navWaypointPosition_t wpPos;
-                gpsLocation_t wpLLH;
+            // // set nearest radar_poi as desired position if in poshold for multirotor:
+            // if (
+            //     posControl.navState == NAV_STATE_POSHOLD_3D_IN_PROGRESS &&
+            //     ARMING_FLAG(ARMED) &&
+            //     (posControl.flags.estPosStatus == EST_TRUSTED) &&
+            //     posControl.gpsOrigin.valid
+            // ){
+            //     navWaypointPosition_t wpPos;
+            //     gpsLocation_t wpLLH;
 
-                int8_t nearest_poi = radarGetNearestPOI();
-                wpLLH.lat = radar_pois[nearest_poi].gps.lat;
-                wpLLH.lon = radar_pois[nearest_poi].gps.lon;
-                wpLLH.alt = radar_pois[nearest_poi].gps.alt / 100; // meters
+            //     int8_t nearest_poi = radarGetNearestPOI();
+            //     wpLLH.lat = radar_pois[nearest_poi].gps.lat;
+            //     wpLLH.lon = radar_pois[nearest_poi].gps.lon;
+            //     wpLLH.alt = radar_pois[nearest_poi].gps.alt / 100; // meters
 
-                geoConvertGeodeticToLocal(&wpPos.pos, &posControl.gpsOrigin, &wpLLH, GEO_ALT_ABSOLUTE);
-                wpPos.pos.z -= gpsSol.llh.alt - getEstimatedActualPosition(Z) - posControl.gpsOrigin.alt; // use GPS_RAW as reference and remove the alt error
-                navSetWaypointFlags_t waypointUpdateFlags = NAV_POS_UPDATE_XY | NAV_POS_UPDATE_Z;
+            //     geoConvertGeodeticToLocal(&wpPos.pos, &posControl.gpsOrigin, &wpLLH, GEO_ALT_ABSOLUTE);
+            //     wpPos.pos.z -= gpsSol.llh.alt - getEstimatedActualPosition(Z) - posControl.gpsOrigin.alt; // use GPS_RAW as reference and remove the alt error
+            //     navSetWaypointFlags_t waypointUpdateFlags = NAV_POS_UPDATE_XY | NAV_POS_UPDATE_Z;
 
-                setDesiredPosition(&wpPos.pos, 0, waypointUpdateFlags);
-            }
+            //     setDesiredPosition(&wpPos.pos, 0, waypointUpdateFlags);
+            // }
         } else
             return MSP_RESULT_ERROR;
         break;
